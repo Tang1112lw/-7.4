618a6b8f2acf6348a4ff75056f05f32c0c5c592c
tang1112lw <1158920686@qq.com>
2026-07-04 18:16:15 +0800

    游离状态需要临时存储再git swich main 再git stash pop

--- 时光印记/study/tools.py
+++ 时光印记/study/tools.py
@@ -32,22 +32,48 @@ origin  https://github.com/Tang1112lw/-.git (push)
 git branch 查看本地仓库分支 
 git push -u origin master:main 上传代码
 
+<<<<<<< Updated upstream
 git push — 推送/上传代码
 -u — 记住这个搭配，以后直接用 git push 就行，不用再打 origin master
 origin — 远程仓库的别名，就是你刚才关联的那个 GitHub 地址
 master — 你本地要推的分支名
+=======
 
-5、分支管理 
-git branch -m master main 改名master 分支为main分支
-git push origin main 暂存main分支
-git push -u origin main -f  强制覆盖提交main分支 
+5、版本控制
+当更新了代码
+git add .
+git commit -m "更新笔记“
+git tag v7.4.1 加上版本标签
+git push origin v7.4.1 上传的同时上传标签
 
+
+>>>>>>> Stashed changes
+
+6.新环境拉取
+下载Git
+配置Git 
+git config --global user.name
+git config --global user.email
+
+创建目录文件接受
+cd Git
+克隆GitHub上传的文件
+git clone GitHub项目链接
+cd 项目名称 7.4
+git tag 查看标签
 
 6、新环境拉去代码  
 下载Git  配置name email
 切换到Git 去GitHub复制连接  
 git clone https://github.com/Tang1112lw/-.git  克隆仓库文件到本地仓库
-
+打开项目编辑更新
+来到项目Git 目录 
+cd 到 -7.4
+git add .
+git commit -m "更新了知识文档"
+git tag v7.4.2
+git push -u origin v7.4.2
+提交7.4.2版本
 """
 
 """
